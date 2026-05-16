08a6f241900caf6e4b2d501fc64e40f210e551ef
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-05-16 22:35:30 +0500

    complete conteact section

--- src/App.jsx
+++ src/App.jsx
@@ -777,6 +777,12 @@ const App = () => {
           </div>
         </div>
       </div>
+      {/* Footer */}
+      <footer>
+        <div className="footer-content">
+          <p>&copy; 2023 Your Company. All rights reserved.</p>
+        </div>
+      </footer>
     </div>
   );
 };

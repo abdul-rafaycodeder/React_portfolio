92e5c9efd1532679be26abb1a9d661ee5f36c76e
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-06-06 20:56:30 +0500

    coding

--- src/index.js
+++ src/index.js
@@ -15,3 +15,7 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+let a = 1;
+let b = a++;
+console.log(a, b)
